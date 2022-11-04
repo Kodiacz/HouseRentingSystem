6bfc2791f16f887c9e55d67537619b7e195723aa
Simeon Yordanov <lookmanche@gmail.com>
2022-11-04 21:38:40 +0200

    created IAgentService and AgentService for AgentController

---
+++ HouseRentingSystem/HouseRentingSystem.Core/Services/AgentService.cs
@@ -0,0 +1,26 @@
+﻿namespace HouseRentingSystem.Core.Services
+{
+    using System.Threading.Tasks;
+
+    using HouseRentingSystem.Core.Contracts;
+    using HouseRentingSystem.Infrastructure.Data.Common;
+    using HouseRentingSystem.Infrastructure.Data.Entities;
+
+    using Microsoft.EntityFrameworkCore;
+
+    public class AgentService : IAgentService
+    {
+        private readonly IRepository repo;
+
+        public AgentService(IRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        public async Task<bool> ExistsById(string userId)
+        {
+            return await repo.All<Agent>()
+                .AnyAsync(a => a.UserId == userId);
+        }
+    }
+}
