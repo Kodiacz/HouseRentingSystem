0b1f2e1b7eee29139be4dd196c487e34cfcab914
Simeon Yordanov <simeon.yordanov@amdaris.com>
2023-03-26 17:28:43 +0300

    created configuration folder for configuring the entities

---
+++ HouseRentingSystem/Data/Configuration/CategoryConfiguration.cs
@@ -0,0 +1,36 @@
+﻿namespace HouseRentingSystem.Data.Configuration
+{
+    using HouseRentingSystem.Data.Entities;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(CreateCategories());
+        }
+
+        private List<Category> CreateCategories()
+        {
+            return new List<Category>()
+            {
+                new Category()
+                {
+                    Id = 1,
+                    Name = "Cottage",
+                },
+                new Category()
+                {
+                    Id = 2,
+                    Name = "Single-Family"
+                },
+                new Category()
+                {
+                    Id = 3,
+                    Name = "Duplex"
+                },
+            };
+        }
+    }
+}
