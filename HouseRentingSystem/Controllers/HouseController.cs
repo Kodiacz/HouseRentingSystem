4c92681a6c2f7a7a1ce2452a76fd1688c24785ad
Simeon Yordanov <simeon.yordanov@amdaris.com>
2023-03-28 00:23:16 +0300

    added new action methods

--- HouseRentingSystem/Controllers/HouseController.cs
+++ HouseRentingSystem/Controllers/HouseController.cs
@@ -1,13 +1,37 @@
-﻿namespace HouseRentingSystem.Controllers
+﻿using HouseRentingSystem.Infrastructure;
+
+namespace HouseRentingSystem.Controllers
 {
     [Authorize]
     public class HouseController : Controller
     {
+        private readonly IHouseService houseService;
+        private readonly IAgentService agentService;
+
+        public HouseController(IHouseService houseService, IAgentService agentService)
+        {
+            this.houseService = houseService;
+            this.agentService = agentService;
+        }
+
         [HttpGet]
         [AllowAnonymous]
-        public IActionResult All()
+        public IActionResult All([FromQuery] AllHousesQueryModel query)
         {
-            return View(new AllHousesQueryModel());
+            var queryResult = this.houseService.All(
+                query.Category,
+                query.SearchTerm,
+                query.Sorting,
+                query.CurrentPage,
+                AllHousesQueryModel.HousesPerPage);
+
+            query.TotalHousesCount = queryResult.TotalHousesCount;
+            query.Houses = queryResult.Houses;
+
+            var houseCategories = this.houseService.AllCategoriesNames();
+            query.Categories = houseCategories;
+
+            return View(query);
         }
 
         [HttpGet]
@@ -21,17 +45,51 @@
         {
             return View(new HouseDetailsViewModel());
         }
-
+        
         [HttpGet]
         public IActionResult Add()
         {
-            return View();
+            if (!this.agentService.ExistById(this.User.Id()!))
+            {
+                ViewData[ErrorMessage] = "First you have to become an agent";
+
+                return RedirectToAction(nameof(AgentController.Become), "Agent");
+            }
+
+            return View(new HouseFormModel()
+            {
+                Categories = this.houseService.AllCategories(),
+            });
         }
 
         [HttpPost]
-        public IActionResult Add(HouseFormModel house)
+        public IActionResult Add(HouseFormModel houseModel)
         {
-            return RedirectToAction(nameof(Details), new {id = "1"});
+            if (!this.agentService.ExistById(this.User.Id()!))
+            {
+                ViewData[ErrorMessage] = "First you have to become an agent";
+
+                return RedirectToAction(nameof(AgentController.Become), "Agent");
+            }
+
+            if (!this.houseService.CategoryExists(houseModel.CategoryId))
+            {
+                this.ModelState.AddModelError(nameof(houseModel.CategoryId),
+                    "Category does not exist");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                houseModel.Categories = this.houseService.AllCategories();
+
+                return View(houseModel);
+            }
+
+            var agentId = this.agentService.GetAgentId(this.User.Id());
+
+            var newHouseId = this.houseService.Create(houseModel, agentId);
+
+            return RedirectToAction(nameof(Details), new { id = newHouseId });
         }
 
         [HttpGet]
