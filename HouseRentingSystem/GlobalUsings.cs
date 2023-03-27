e2ced3f46e136503c914d9933c08b9b015a11ac8
Simeon Yordanov <simeon.yordanov@amdaris.com>
2023-03-27 11:20:17 +0300

    created global usings file

---
+++ HouseRentingSystem/GlobalUsings.cs
@@ -0,0 +1,23 @@
+﻿global using System.Diagnostics;
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Migrations;
+global using Microsoft.EntityFrameworkCore.Infrastructure;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+global using HouseRentingSystem.Data;
+global using HouseRentingSystem.Models;
+global using HouseRentingSystem.Services;
+global using HouseRentingSystem.Interfaces;
+global using HouseRentingSystem.Models.Home;
+global using HouseRentingSystem.Models.House;
+global using HouseRentingSystem.Data.Entities;
+global using HouseRentingSystem.Data.Configuration;
+global using HouseRentingSystem.Data.Entities.Configuration;
