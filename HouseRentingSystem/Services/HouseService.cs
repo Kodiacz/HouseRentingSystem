91811ffcd335af3963c55c6ac5cc0b25e46ef2f8
Simeon Yordanov <simeon.yordanov@amdaris.com>
2023-03-27 11:22:00 +0300

    created Service directory and one service file

---
+++ HouseRentingSystem/Services/HouseService.cs
@@ -0,0 +1,29 @@
+﻿namespace HouseRentingSystem.Services
+{
+    public class HouseService : IHouseService
+    {
+        private readonly HouseRentingDbContext dbContext;
+
+        public HouseService(HouseRentingDbContext context)
+        {
+            this.dbContext = context;
+        }
+
+        public IEnumerable<HouseIndexServiceModel> LastThreeHouses()
+        {
+            return this.dbContext
+                .Houses
+                .OrderByDescending(h => h.Id)
+                .Select(h => new HouseIndexServiceModel()
+                {
+                    Id = h.Id,
+                    Title = h.Title,
+                    ImageUrl = h.ImageUrl,
+                })
+                .Take(3)
+                .ToList();
+        }
+    }
+}
+
+
