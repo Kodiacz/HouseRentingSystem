6ac3f5af0d097bc8a338542bac2d3f886d12191c
Simeon Yordanov <simeon.yordanov@amdaris.com>
2023-03-30 10:49:29 +0300

    imlemented StatisticService methods

---
+++ HouseRentingSystem/Models/Statistics/StatisticsServiceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HouseRentingSystem.Models.Statistics
+{
+    public class StatisticsServiceModel
+    {
+        public int TotalHouses { get; init; }
+
+        public int TotalRents { get; init; }
+    }
+}
---
+++ HouseRentingSystem/Services/StatisticsService.cs
@@ -0,0 +1,31 @@
+﻿namespace HouseRentingSystem.Services
+{
+    public class StatisticsService : IStatisticsService
+    {
+        private readonly HouseRentingDbContext dbContext;
+
+        public StatisticsService(HouseRentingDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public StatisticsServiceModel Total()
+        {
+            var totalHouses = this.dbContext
+                .Houses
+                .Where(h => !h.IsDeleted)
+                .Count();
+
+            var totalRents = this.dbContext
+                .Houses
+                .Where(h => h.RenterId != null && !h.IsDeleted)
+                .Count();
+
+            return new StatisticsServiceModel()
+            {
+                TotalHouses = totalHouses,
+                TotalRents = totalRents,
+            };
+        }
+    }
+}
