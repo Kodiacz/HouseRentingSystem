c6df2d7c0411737c66eea25bd08c27e59b833f99
Simeon Yordanov <simeon.yordanov@amdaris.com>
2023-03-30 10:47:58 +0300

    created Api Statistic controller

---
+++ HouseRentingSystem/Controllers/API/StatisticsApiController.cs
@@ -0,0 +1,20 @@
+﻿namespace HouseRentingSystem.Controllers.API
+{
+    [ApiController]
+    [Route("api/statistics")]
+    public class StatisticsApiController : ControllerBase
+    {
+        private readonly IStatisticsService statisticsService;
+
+        public StatisticsApiController(IStatisticsService statisticsService)
+        {
+            this.statisticsService = statisticsService;
+        }
+
+        [HttpGet]
+        public StatisticsServiceModel GetStatistics()
+        {
+            return this.statisticsService.Total();
+        }
+    }
+}
