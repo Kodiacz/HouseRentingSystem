76a7dfab78dc0065ea00754e7df397ebd781b0ac
Simeon Yordanov <simeon.yordanov@amdaris.com>
2023-03-31 18:25:39 +0300

    added new agent

--- HouseRentingSystem/Data/Configuration/AgentConfiguration.cs.cs
+++ HouseRentingSystem/Data/Configuration/AgentConfiguration.cs.cs
@@ -4,12 +4,19 @@
     {
         public void Configure(EntityTypeBuilder<Agent> builder)
         {
-            builder.HasData(new Agent()
-            {
-                Id = 1,
-                PhoneNumber = "+359888888888",
-                UserId = "dea12856-c198-4129-b3f3-b893d8395082"
-            });
+            builder.HasData(
+                new Agent()
+                {
+                    Id = 1,
+                    PhoneNumber = "+359888888888",
+                    UserId = "dea12856-c198-4129-b3f3-b893d8395082",
+                },
+                new Agent()
+                {
+                    Id = 5,
+                    PhoneNumber = "+359123456789",
+                    UserId = "bcb4f072-ecca-43c9-ab26-c060c6f364e4",
+                });
         }
     }
 }
