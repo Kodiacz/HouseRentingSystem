5a2c665603c7301c2d037ece515b7660e99d2891
Simeon Yordanov <simeon.yordanov@amdaris.com>
2023-04-02 10:54:07 +0300

    created new models for areas

---
+++ HouseRentingSystem.Web/Areas/Admin/Models/MyHouseViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace HouseRentingSystem.Web.Areas.Admin.Models
+{
+    public class MyHouseViewModel
+    {
+        public MyHouseViewModel()
+        {
+            this.AddedHouses = new List<HouseServiceModel>();
+            this.RentedHouses = new List<HouseServiceModel>();
+        }
+
+        public IEnumerable<HouseServiceModel> AddedHouses { get; set; }
+
+        public IEnumerable<HouseServiceModel> RentedHouses { get; set; }
+    }
+}
