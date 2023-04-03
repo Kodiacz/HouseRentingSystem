88c2479312710d77dadd1867b6cd846370453566
Simeon Yordanov <simeon.yordanov@amdaris.com>
2023-04-03 21:18:38 +0300

    implmented method all

---
+++ HouseRentingSystem.Services/Models/User/UserServiceModel.cs
@@ -0,0 +1,11 @@
+﻿namespace HouseRentingSystem.Services.Models.User
+{
+    public class UserServiceModel
+    {
+        public string Email { get; init; } = null!;
+
+        public string FullName { get; init; } = null!;
+
+        public string PhoneNumber { get; init; } = null!;
+    }
+}
--- HouseRentingSystem.Services/Services/UserService.cs
+++ HouseRentingSystem.Services/Services/UserService.cs
@@ -9,6 +9,36 @@
             this.dbContext = dbContext;
         }
 
+        public IEnumerable<UserServiceModel> All()
+        {
+            var allUsers = new List<UserServiceModel>();
+
+            var agents = this.dbContext
+                .Agents
+                .Include(ag => ag.User)
+                .Select(ag => new UserServiceModel()
+                {
+                    Email = ag.User.Email,
+                    FullName = ag.User.FirstName + " " + ag.User.LastName,
+                    PhoneNumber = ag.PhoneNumber,
+                });
+
+            allUsers.AddRange(agents);
+
+            var users = this.dbContext
+                .Users
+                .Where(u => !this.dbContext.Agents.Any(ag => ag.UserId == u.Id))
+                .Select(u => new UserServiceModel()
+                {
+                    Email = u.Email,
+                    FullName = u.FirstName + " " + u.LastName,
+                });
+
+            allUsers.AddRange(users);
+            
+            return allUsers;
+        }
+
         public string? UserFullName(string userId)
         {
             var user = this.dbContext.Users.Find(userId);
