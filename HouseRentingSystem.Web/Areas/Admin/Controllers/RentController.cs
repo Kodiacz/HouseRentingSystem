e8e18665a715c3bf22565f6767fbe99e57cdf5e9
Simeon Yordanov <simeon.yordanov@amdaris.com>
2023-04-03 21:22:48 +0300

    new rent controller

---
+++ HouseRentingSystem.Web/Areas/Admin/Controllers/RentController.cs
@@ -0,0 +1,19 @@
+﻿namespace HouseRentingSystem.Web.Areas.Admin.Controllers
+{
+    public class RentController : AdminController
+    {
+        private readonly IRentService rentService;
+
+        public RentController(IRentService rentService)
+        {
+            this.rentService = rentService;
+        }
+
+        [Route("Rent/All")]
+        public ActionResult All()
+        {
+            var rents = this.rentService.All();
+            return View(rents);   
+        }
+    }
+}
