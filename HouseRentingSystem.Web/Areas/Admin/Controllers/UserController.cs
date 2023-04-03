e2620822ce0d4db694911f93b4f94ed2c0c209ef
Simeon Yordanov <simeon.yordanov@amdaris.com>
2023-04-03 21:23:05 +0300

    new user controller

---
+++ HouseRentingSystem.Web/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿namespace HouseRentingSystem.Web.Areas.Admin.Controllers
+{
+    public class UserController : AdminController
+    {
+        private readonly IUserService userService;
+
+        public UserController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [Route("Users/All")]
+        public IActionResult All()
+        {
+            var users = this.userService.All();
+            return View(users);
+        }
+    }
+}
